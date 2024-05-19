74f91c6906315fa2660c1bd1436e6349cc559e85
Saroz <31454493+spsaroj@users.noreply.github.com>
2024-05-18 22:54:01 -0500

    final commit

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # data-hackfest
 
+This application is hosted in render.com.
 
+### Steps to run the app on local
+
+cd to DATA-HACKFEST folder.
+
+Create a virtual environment:
 python -m venv < venv- name >
-pip install fastapi uvicorn
+
+Run the virtual environment:
+./< venv- name >/Scripts/activate
+
+Install the requirements:
+pip install -r requirements.txt
+
+Run the app:
+uvicorn main:app --reload
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import csv
 
 app = FastAPI()
+
+allowed_origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,  # Optional, allows cookies for authenticated requests
+    allow_methods=["*"],  # Optional, allows all HTTP methods
+    allow_headers=["*"],  # Optional, allows all headers
+)
 
 class Meditation(BaseModel):
     description: str
@@ -14,16 +26,15 @@ class MeditationData(BaseModel):
 
 # Load your CSV data into a dictionary
 meditation_data = {}
-with open("meditation_techniques.csv") as file:
-    # Assuming the first row has headers (Name, Description, Duration, Instructions)
-    headers = next(file).strip().split(",")
-    for line in file:
-        data = dict(zip(headers, line.strip().split(",")))
-        meditation_data[data["Name"]] = Meditation(
-            description=data["Description"],
-            duration=data["Duration"],
-            instructions=data["Instructions"],
-        )
+
+with open("meditation_techniques.csv", newline='', encoding='utf-8-sig') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        meditation_data[row["Name"]] = {
+            "description": row["Description"],
+            "duration": row["Duration"],
+            "instructions": row["Instructions"]
+        }
 
 @app.get("/names")
 async def get_all_names():
@@ -34,9 +45,6 @@ async def get_meditation_details(name: str):
     meditation = meditation_data.get(name)
     if meditation is None:
         return {"error": f"Meditation with name '{name}' not found"}
+
+    print(meditation)
     return meditation
-
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
